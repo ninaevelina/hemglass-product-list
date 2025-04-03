51162f9a2636158137fe49a72a0a08b863337a05
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2025-04-03 11:36:35 +0200

    refactor: removes useEffect for data fetching

--- src/app/ui/products/products.tsx
+++ src/app/ui/products/products.tsx
@@ -1,41 +1,53 @@
-"use client";
-
-import { Product } from "@/app/lib/definitions";
-import { fetchAllProducts } from "@/app/lib/services/fetch-all-products";
-import { useEffect, useState } from "react";
-import ProductCard from "../product-card/product-card";
+import Image from "next/image";
 import "./products.scss";
+import { fetchAllProducts } from "@/app/lib/services/fetch-all-products";
 
-export default function Products() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const products = await fetchAllProducts();
-        setProducts(products.items);
-        setIsLoading(false);
-        console.log("Products:", products.items);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
-    fetchData();
-  }, []);
+export default async function Products() {
+  const data = await fetchAllProducts();
+  const products = data.items;
 
   return (
     <section>
-      {isLoading ? (
-        <div>Loading</div>
-      ) : (
-        <ul className="productlist">
-          {products.map((product) => (
-            <ProductCard key={product.productId} product={product} />
-          ))}
-        </ul>
-      )}
+      <ul className="productlist">
+        {products.map((product) => (
+          <li className="list-item" key={product.productId}>
+            <article className="product-card">
+              <div
+                className="product-card__image-container"
+                style={{
+                  backgroundImage: product.backgroundImgCard?.source
+                    ? `url(${product.backgroundImgCard.source})`
+                    : "none",
+                  backgroundSize: "cover",
+                }}
+              >
+                <Image
+                  src={product.productImg.source}
+                  alt={product.productName}
+                  loading="lazy"
+                  sizes="(min-width: 768px) 25vw, 50vw"
+                  style={{
+                    objectFit: "contain",
+                    maxHeight: "100%",
+                    padding: "40px 0px",
+                  }}
+                  fill
+                />
+              </div>
+              <div className="product-card__details">
+                <p>{product.productName}</p>
+                <p>{product.customProductVariantMappings.packageSize} st</p>
+                <p>{product.priceWithTax.max} kr</p>
+              </div>
+              <div className="product-card__button-container">
+                <button className="button-primary">
+                  Lägg till i varukorgen
+                </button>
+              </div>
+            </article>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
