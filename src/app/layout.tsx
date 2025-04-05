27aa6be1a6f28c7fd6fe053ef9439354ce40b72b
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2025-04-05 10:56:16 +0200

    feat: adds scaffolding for footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/_globals.scss";
 import Header from "./ui/shared/header/header";
+import Footer from "./ui/shared/footer/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/app/ui/shared/footer/footer.tsx
@@ -0,0 +1,10 @@
+import "./footer.scss";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__column-top"></div>
+      <div className="footer__column-bottom"></div>
+    </footer>
+  );
+}
