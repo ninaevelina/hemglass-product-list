bab0d05c5b793cbc8826c020425059ecdd7839c6
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2025-04-06 14:21:53 +0200

    refactor: adds content and updates styles for the footer component

--- src/app/ui/shared/footer/footer.tsx
+++ src/app/ui/shared/footer/footer.tsx
@@ -3,8 +3,8 @@ import "./footer.scss";
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__column-top"></div>
-      <div className="footer__column-bottom"></div>
+      <div className="footer__column-top">Footer top column</div>
+      <div className="footer__column-bottom">Footer bottom column</div>
     </footer>
   );
 }
